7f05eb260c59d89ddac4987282780a153a53d20a
Emlay9 <46056494+Emlay9@users.noreply.github.com>
2019-01-31 08:02:29 -0700

    Add files via upload

---
+++ More Inheritance/More Inheritance/Inheritance/AbstractLock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LanguageReview.CSharp.Inheritance
+{
+    // You can't create an object instance of an abstract class
+    public abstract class AbstractLock
+    {
+        public bool IsLocked { get; protected set; }
+        public virtual void Lock()
+        {
+            IsLocked = true;
+        }
+        public abstract void Unlock(params int[] keyNumbers);
+    }
+}
