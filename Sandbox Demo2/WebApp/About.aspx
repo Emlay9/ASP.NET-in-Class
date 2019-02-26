82d9bf72173a9109fbd410196b66423c172d52d7
Emlay9 <emilylthomas9@gmail.com>
2019-02-26 09:48:38 -0700

    added little initial reverse name app

--- Sandbox Demo2/WebApp/About.aspx
+++ Sandbox Demo2/WebApp/About.aspx
@@ -3,5 +3,14 @@
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
     <h2><%: Title %>.</h2>
     <h3>Your application description page.</h3>
-    <p>Use this area to provide additional information.</p>
+    <p>Use this area to provide additional information. Enter your name and click the button</p>
+
+    <%-- The ID attribute will generate a field name in the code-behind class that allows me to programatically interact with the asp.net server control --%>
+    <asp:Label ID="MessageLabel" runat="server" />
+
+    <asp:TextBox ID="FullName" runat="server" />
+    <%-- A LinkButton will look like a link, render as an <a>, but work like a button --%>
+    <asp:LinkButton ID="GetInitials" runat="server" CssClass="btn btn-primary" Text="Get Your Initials" onClick="GetInitials_Click"/>
+    <asp:Label ID="Initials" runat="server" />
+    <asp:LinkButton ID="ReverseName" runat="server" CssClass="btn btn-default" Text="Spell it backwards" Visible="false" OnClick="ReverseName_Click" />
 </asp:Content>
--- Sandbox Demo2/WebApp/About.aspx.cs
+++ Sandbox Demo2/WebApp/About.aspx.cs
@@ -9,9 +9,57 @@ namespace WebApp
 {
     public partial class About : Page
     {
+        //Page_Load is a stage of processing the page where you get to set up aspects of the page's content before any of the events of your
+        // page's controls are processed.
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(!IsPostBack) // !IsPostBack means a GET request
+            {
+                //sets the text of the MessageLabel label
+                MessageLabel.Text = "Tell us about yourself! You can trust us!";
+                // Label    .Sting
 
+            }
+            else
+            {
+                MessageLabel.Text = String.Empty;
+            }
+        }
+
+        // this method was created when we added the onClick event in the About.aspx page
+        protected void GetInitials_Click(object sender, EventArgs e)
+        {
+            //this method is what gets called when the <asp:LinkButton ID="GetInitials" ... is clicked.
+            //We can say that this method "handles" the click event of that button/link.
+
+            //FullName.Text gets the text that was entered into the textbox
+            //creates an array of strings (spacebar as delimeter)
+            var nameParts = FullName.Text.Split(' ');
+            //Construct the initials by taking the first character of each word part
+            string result = "";
+            foreach(var part in nameParts)
+            {
+                if (!string.IsNullOrWhiteSpace(part))
+                {
+                    result += $"{part[0]}.";
+                }
+            }
+            //Place the result in the label on the form 
+            Initials.Text = result;
+
+            if (string.IsNullOrWhiteSpace(result))
+            {
+                MessageLabel.Text = "I can't get initials from empty text...";
+                MessageLabel.CssClass = "bg-info";
+            }
+            else
+                ReverseName.Visible = true;
+        }
+
+        protected void ReverseName_Click(object sender, EventArgs e)
+        {
+            string reversed = string.Join("", FullName.Text.ToArray().Reverse());
+            MessageLabel.Text = $"Your name reversed is <b>{reversed}</b>";
         }
     }
 }
