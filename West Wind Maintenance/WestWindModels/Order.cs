4b7adcbee16314b9e7ac46b2bc982e44b24105b2
Emlay9 <46056494+Emlay9@users.noreply.github.com>
2019-03-29 11:43:23 -0600

    added entity class customer and employee
    and some comments

--- West Wind Maintenance/WestWindModels/Order.cs
+++ West Wind Maintenance/WestWindModels/Order.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WestWindModels
 {
-    class Order
+    [Table("Orders")]
+    public class Order
     {
+        //NOTE: Primitive value types can be made "nullable" by adding a ? after the datatype
+        [Key]                                                           // SQL Types
+        public int OrderID { get; set; }                                // int      NOT NULL
+        public int SalesRepID { get; set; }                             // int      NOT NULL
+        public string CustomerID { get; set; }                          // string    
+        public DateTime? OrderDate { get; set; }
+        public DateTime? RequiredDate { get; set; }
+        public decimal? Freight { get; set; }
+        // NOTE: Strings are reference types, and can store a null value by definition
+        public string ShipName { get; set; }
+        public string ShipAddress { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipRegion { get; set; }
+        public string ShipPostalCode { get; set; }
+        public string ShipCountry { get; set; }
+        public string Comments { get; set; }
     }
 }
